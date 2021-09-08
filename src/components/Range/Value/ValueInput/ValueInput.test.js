45af19067650f29d9e639e985cfed1e25fd5c4fe
Nahuel Carreiro <nahuelcarreiro@gmail.com>
2021-09-08 15:19:50 +0200

    Fixed ValueInput test.

--- src/components/Range/Value/ValueInput/ValueInput.test.js
+++ src/components/Range/Value/ValueInput/ValueInput.test.js
@@ -14,7 +14,7 @@ test("returns ValueInput with initial value 40", () => {
 });
 
 test("edits ValueInput number typing 60, with initial value 40", () => {
-  render(<ValueInput type="min" initialValue={40} />);
+  render(<ValueInput type="min" initialValue={40} setEdit={() => {}} />);
   const inputElement = screen.getByTestId("range-value-min-input");
   fireEvent.change(inputElement, { target: { value: 60 } });
   expect(inputElement.value).toBe("60");
